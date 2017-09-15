97a7ffc05238c5e151402f4e0697507087ca4e16
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2017-09-15 15:37:43 +0300

    Initial commit

---
+++ src/main/java/com/mytests/springboot/webapp/defaults/freemarker/defaultfreemarkertest0/DefaultFreemarkerTest0Application.java
@@ -0,0 +1,12 @@
+package com.mytests.springboot.webapp.defaults.freemarker.defaultfreemarkertest0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefaultFreemarkerTest0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefaultFreemarkerTest0Application.class, args);
+	}
+}
